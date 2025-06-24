854a998233e49c3609a8b4cb659140a1b9b39dbb
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-24 16:00:50 -0500

    updated

--- app/register.tsx
+++ app/register.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Page() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Register</Text>
-            <link href="/login">
-                <Text>Log in to existing account</Text>
-            </link>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Register</Text>
+
+      <Link href="/login">
+        <Text>Log in to existing account</Text>
+      </Link>
+    </View>
+  );
 }
