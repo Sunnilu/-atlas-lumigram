f3fb0fdaedc3adb908cd760830f16fe06e3f6f0b
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-24 13:28:34 -0500

    added a post tab

---
+++ app/(tabs)/add-post.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Page() {
+    return (
+        <view style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <text>Add Post{'>'}</text>
+        </view>
+    );
+}
