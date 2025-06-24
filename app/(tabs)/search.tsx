e005282a862cb58749b1304fd3a45ec09b975c58
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-24 15:07:20 -0500

    added a linik href for profile

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -4,6 +4,7 @@ export default function Page() {
     return (
         <view style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <text>Search</text>
+            <link href="/profile/1">Profile 1</link>
         </view>
     );
 }
