a0986e8ea104757ea2656c6145481626eb26dce7
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-24 16:37:30 -0500

    changed header to My profile

--- app/(tabs)/profile/[id].tsx
+++ app/(tabs)/profile/[id].tsx
@@ -1,11 +1,16 @@
 import { useLocalSearchParams } from 'expo-router';
 import { Text, View } from 'react-native';
+import { Stack } from 'expo-router';
 
 export default function Page() {
-    const { id } = useLocalSearchParams();
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>User Profile for: id</Text>
-        </View>
-    );
+  const { id } = useLocalSearchParams();
+
+  return (
+    <>
+      <Stack.Screen options={{ title: 'My Profile' }} />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>User Profile for: {id}</Text>
+      </View>
+    </>
+  );
 }
