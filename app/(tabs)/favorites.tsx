23454260ebb87e1a84879c4356ddca0b7c16f0a8
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-24 13:33:40 -0500

    add a favorites

---
+++ app/(tabs)/favorites.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Page() {
+    return (
+        <view style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <text>favorites</text>
+        </view>
+    );
+}
