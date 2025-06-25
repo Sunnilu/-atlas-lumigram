bb4d203009d5c940d102b22d6db98b7e734bb2d8
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-24 19:13:43 -0500

    updated

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,11 +1,9 @@
-// profile.tsx 
 import { Text, View } from 'react-native';
 
-export default function Page() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Profile</Text>
-        </View>
-    );
-    }
-// This is a simple profile page component for a React Native application.
+export default function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>My Profile</Text>
+    </View>
+  );
+}
