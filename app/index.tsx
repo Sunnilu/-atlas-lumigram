49c1e95509477419b243a6a762c3d1e7cc54b0a0
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-24 19:36:57 -0500

    the /tab)/ was throwing an error

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
-import { Redirect } from "expo-router";
+// app/index.tsx
+import { Redirect } from 'expo-router';
 
-export default function Page() {
-    return <Redirect href="/login" />;
-    }
+export default function Index() {
+  return <Redirect href="/(tabs)" />;
+}
