3e791a12ce6ef7b550c3892fb9c7fba34664820a
Suntha <Suntha.Lucas@atlasschool.com>
2025-06-30 10:41:02 -0500

    added imports storage, firestore firebase auth

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,19 @@
-// lib/firebase.ts
 import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAow7_HkDwFMRyWKPt-CDNB2aM_dDHZNfY",
-  authDomain: "atlas-lumigram-d7ebe.firebaseapp.com",
-  projectId: "atlas-lumigram-d7ebe",
-  storageBucket: "atlas-lumigram-d7ebe.appspot.com",
-  messagingSenderId: "823121526760",
-  appId: "1:823121526760:web:4d2789cd3adb1a0716ee5d",
-  measurementId: "G-B6JQ611XHP",
+  apiKey: 'YOUR_API_KEY',
+  authDomain: '...',
+  projectId: '...',
+  storageBucket: '...',
+  messagingSenderId: '...',
+  appId: '...',
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
